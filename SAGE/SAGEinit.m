8d1ad11c039830efff90e1238e13656a744e605f
lyao-tudelft <lichenyao94@gmail.com>
2017-03-30 20:27:07 +0200

    Minor change on SAGEinit

--- SAGE/SAGEinit.m
+++ SAGE/SAGEinit.m
@@ -1,8 +1,7 @@
-function [ output_args ] = SAGEinit( y, L, u, sys )
+function theta_out = SAGEinit( y, u, sys )
 % Initialization phase of SAGE algorithm. See details in SAGE paper IV.B
 %
 % y: superimposed received noise corrupted signal
-% L: estimated number of paths
 % u: transmitted signal
 % sys: system information
 
@@ -11,22 +10,25 @@ fs = sys.fs;
 M = sys.M;
 d = sys.d;
 I = sys.I;
+L = sys.L;
 
 lena = round(Ta*fs);
 lenf = round(Tf*fs);
 
 zero = cell(L,1);
+one = cell(L,1);
 for i = 1:L
     zero{i} = 0;
+    one{i} = 0;
 end
 
-theta = struct('tau', zero, 'phi', zero, 'fdopp', zero, 'amp', zero);   % Initialize estimated parameters
-
+theta = struct('tau', zero, 'phi', zero, 'fdopp', zero, 'amp', one);   % Initialize estimated parameters
+theta_temp = theta;
 
 for miu = -(L-1):0
-    l = M+miu;
-
-    xel_last = xel( y, l, theta(l), u, sys );     % Estimated x with estimated paramters from the last iteration
+    l = L+miu;
+    
+    xel_last = xel( y, l, theta, u, sys );     % Estimated x with estimated paramters from the last iteration
 
     %% Estimate tau
     taumax = 2e-6;
@@ -42,28 +44,32 @@ for miu = -(L-1):0
         ud(delay+1:end) = u(1:end-delay);   % Delayed version of transmitted sinal due to path delay
         
         for m = 1:M
-            temps = conj(ud).*xel_last(m,:);
+
+            temps = conj(ud)'.*xel_last(m,:);
             for i = 1:I
                 %temp = (sum(conj(ud((i-1)*lenf+1:(i-1)*lenf+1+lena)).*xel_last(m,(i-1)*lenf+1:(i-1)*lenf+1+lena)))^2;
-                temp = (sum(temps((i-1)*lenf+1:(i-1)*lenf+1+lena)))^2;
+                temp = abs(sum(temps((i-1)*lenf+1:(i-1)*lenf+1+lena)))^2;
                 tempsum(cou) = tempsum(cou)+temp;
             end
             tempsumm(cou) = tempsumm(cou) + tempsum(cou);
         end
         cou = cou + 1;
     end
-    
+    tempsum;
     % Find the tau that maximize it
     [~,Itau] = max(tempsumm);
-    tau_e = 0+(Itau-1)*(taumax/20);    % The delay estimation
+    Itau;
+    tau = linspace(0,taumax,20);
+    tau_e = tau(Itau(1));    % The delay estimation
+    theta_temp(l).tau = tau_e;
     
     %% Estimate phi
-    delay_a = round(theta(l).tau*fs);
+    delay_a = round(tau_e*fs);
     ud(delay_a+1:end) = u(1:end-delay_a);   % Delayed version of transmitted signal with ...
                                             % estimated delay from the last iteration
     
     phimax = 2*pi;
-    phi = linspace(0,phimax,90);
+    phi = linspace(0,phimax,5);
     c = zeros(M,length(phi));
     sumphim = zeros(length(phi),1);
     
@@ -72,23 +78,49 @@ for miu = -(L-1):0
         for i = 1:M
             c(i,j) = exp(1i*2*pi*(i-1)*d*cos(phi(j)));
         end
-        
-        sumphi = conj(ud).*c(:,j).'*xel_last;
+
+        sumphi = conj(ud)'.*((c(:,j).')*xel_last);
         for i = 1:I
-            tempphi = (sum(sumphi((i-1)*lenf+1:(i-1)*lenf+lena)))^2;
+            tempphi = abs(sum(sumphi((i-1)*lenf+1:(i-1)*lenf+lena)))^2;
             sumphim(j) = sumphim(j)+tempphi;
         end
     end
-    
+    sumphim;
     % Find the phi that maximize it
     [~, Iphi] = max(sumphim);
-    phi_e = 0+(Iphi-1)*(phimax/90);     % The phi estimation
+    phi_e = phi(Iphi(1));     % The phi estimation
+    theta_temp(l).phi = phi_e;
     
     %% Estimate doppler frequency
     fdmax = 10;
     fd = linspace(0,fdmax,10);  % Doppler frequency
-    z = zfun( xel, c, u, l, thetal, sys );
+    
+    cfd = zeros(M,1);
+    for m = 1:M
+        cfd(m) = exp(1i*2*pi*(m-1)*d*cos(theta_temp(l).phi));
+    end
+    
+    theta_t = theta_temp;
+    z = zeros(length(fd),1);
+    for i = 1:length(fd)
+        theta_t(1).fdopp = fd(i);
+        z(i) = abs(zfun(xel_last, cfd, u, l, theta_t(l), sys));
+    end
+    
+    z;
+    [~, Ifd] = max(z);
+    Ifd;
+    fd_e = fd(Ifd(1));
+    theta_temp(l).fdopp = fd_e;
+    
+    %% Estimate Amplitude
+    Pu = sum(u.^2)/length(u);
+    a_e = 1/(I*norm(c)^2*Ta*Pu)*zfun(xel_last, cfd, u, l, theta_temp(l),sys);
+    theta_temp(l).amp = a_e;
+    
 end
+
+theta_out = theta_temp;
 
 end
 
--- SAGE/s.m
+++ SAGE/s.m
@@ -3,6 +3,9 @@ function out = s( theta, u, sys )
 %
 % theta: parameters of specified path
 % u: transmitted signal
+%
+% out: Received signal without noise corruption. Each row corresponds to a
+% receiver.
 
 M = sys.M;
 d = sys.d;
@@ -12,12 +15,12 @@ ud = zeros(length(u),1);
 delay = round(theta.tau*fs);
 ud(delay+1:end) = u(1:end-delay);   % Delayed version of transmitted sinal due to path delay
 
-t = (1:1/fs:length(u)/fs)';
+t = (0:1/fs:(length(u)-1)/fs)';
 out = zeros(M, length(u));
 c = zeros(M,1);
 for i = 1:M
     c(i) = exp(1i*2*pi*(i-1)*d*cos(theta.phi));
-    out(i,:) = c(i)*theta.amp*exp(1i*2*pi*theta.fdopp.*t).*ud;
+    out(i,:) = c(i)*theta.amp*exp(1i*2*pi*theta.fdopp*t).*ud;
 end
 
 end
